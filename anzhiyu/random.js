075736960c96cdc36211c6e2b7bd62cc5f3d581e
kingleonardo999 <1990408695@qq.com>
2025-03-27 22:01:52 +0800

    Site updated: 2025-03-27 22:01:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/15/My-New-Post/","2025/03/15/hello-world/"];function toRandomPost(){
+var posts=["2025/03/15/hello-world/","2025/03/15/My-New-Post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
