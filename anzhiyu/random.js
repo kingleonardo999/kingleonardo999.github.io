5d7b4bb556931ba629721ed9dad2156ecbcc1373
kingleonardo999 <1990408695@qq.com>
2025-06-02 20:47:56 +0800

    Site updated: 2025-06-02 20:47:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/15/hello-world/","2025/03/15/My-New-Post/"];function toRandomPost(){
+var posts=["2025/03/15/hello-world/","2025/06/02/backend-go/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
