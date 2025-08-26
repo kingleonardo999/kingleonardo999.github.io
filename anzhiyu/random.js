d1947422fcd3e338483739c453fe3508d5fd598d
kingleonardo999 <1990408695@qq.com>
2025-08-26 23:20:18 +0800

    Site updated: 2025-08-26 23:20:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/02/WebBlog/","2025/06/02/backend-go/","2025/06/03/backend-gin/","2025/06/03/go-redis/","2025/06/03/backend-gorm/","2025/06/28/go-tags/","2025/06/03/viper/","2025/03/15/hello-world/"];function toRandomPost(){
+var posts=["2025/06/03/backend-gin/","2025/06/02/WebBlog/","2025/06/02/backend-go/","2025/03/15/hello-world/","2025/06/03/backend-gorm/","2025/06/03/go-redis/","2025/06/03/viper/","2025/06/28/go-tags/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
